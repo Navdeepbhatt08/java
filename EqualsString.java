ea848a21183c654f51d0656dcf29507cb4721ecf
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-08-20 21:29:29 +0530

    Refactor EqualsString class to demonstrate string equality checks

--- EqualsString.java
+++ EqualsString.java
@@ -1,14 +1,16 @@
-
 class EqualsString 
 {
     public static void main(String[] args)
 
 {
+    String s1 = "XYZ";
+    boolean t = s1.equals("XYZ"); //true
+    boolean t1 = s1.equals("xyz");  //false
+    boolean t2 = s1.equalsIgnoreCase("xyz");  //true
 
 
-    String s1 = "Aavya";
-    boolean t = s1.equalsIgnoreCase("aavya");
-
-     System.out.println(t);
+    System.out.println(t);
+    System.out.println(t1);
+    System.out.println(t2);
 }
 }
